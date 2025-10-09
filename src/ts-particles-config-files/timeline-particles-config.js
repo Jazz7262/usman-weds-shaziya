f384a5281685e0d60c1f772118037a61005a7dc5
Jazz7262 <116513312+Jazz7262@users.noreply.github.com>
2025-10-09 12:49:58 +0530

    Update timeline-particles-config.js

--- src/ts-particles-config-files/timeline-particles-config.js
+++ src/ts-particles-config-files/timeline-particles-config.js
@@ -29,7 +29,7 @@ const timelineParticlesConfig = {
     particles: {
         color: {
             // value: "#002e20",
-            value: "#002e2000",
+            value: "#ffffff00",
             opacity: 0.4,
         },
         // links: {
